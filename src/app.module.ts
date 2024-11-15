bb00b5513e2b22f8915a0d0172693e1d90a2c7eb
zhangjielong <jielong.zhang@apuqi.com>
2024-11-15 18:18:09 +0800

    part of rss

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './common/config/config.module'; // Import the ConfigModule
 import { IpIntervalModule } from './common/ipInterval/ip.interval.module'
+import { GlobalJwtClientsModule } from './common/global/jwt';
+import { GlobalLogClientsModule } from './common/global/log';
+import { GlobalUserClientsModule } from './common/global/user';
+import { TaskModule } from './task/module';
+import { LogModule } from './log/module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, GlobalJwtClientsModule, GlobalLogClientsModule, GlobalUserClientsModule, TaskModule, LogModule],
 })
 export class AppModule { }
