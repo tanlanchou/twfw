2f60219ed96360e23e5786db983b00c971fbaff1
zhangjielong <jielong.zhang@apuqi.com>
2024-12-12 11:10:50 +0800

    update acl to consul

--- run.bat
+++ run.bat
@@ -1,1 +1,5 @@
-docker run -d --name log_v0.1.0_container -p 8103:8103 --env CONSUL_HOST=%CONSUL_HOST% --restart always log_v0.1.0
+echo CONSUL_HOST
+echo %CONSUL_HOST%
+echo CONSUL_TOKEN
+echo %CONSUL_TOKEN%
+docker run -d --name log_v0.1.0_container -p 8103:8103 --env CONSUL_HOST=%CONSUL_HOST% --env CONSUL_TOKEN=%CONSUL_TOKEN% --restart always log_v0.1.0
