378b4e183b25f3f9bc2ea1dc8599bd10ea0ae2fb
tommy <tlstommy@outlook.com>
2025-01-24 18:48:09 +0800

    fix bug: remove logger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { ConfigService } from './common/config/config.service';
-import { WinstonModule } from 'nest-winston';
-import * as winston from 'winston';
 
 async function bootstrap() {
-
   process.env.CONFIG_NAME = "log";
   process.env.VERSION = "v0.1.0";
   if (!process.env.NODE_ENV) {
@@ -21,22 +18,7 @@ async function bootstrap() {
     options: {
       host: config.microservice.host,
       port: config.microservice.port,
-    },
-    logger: WinstonModule.createLogger({
-      transports: [
-        new winston.transports.File({
-          filename: 'application.log',
-          maxsize: 200 * 1024 * 1024, // 200 MB
-          maxFiles: 5, // Keep a maximum of 5 log files
-          tailable: true, // Ensure the log files are rotated
-          zippedArchive: true, // Compress the rotated files
-          format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.json()
-          ),
-        }),
-      ],
-    }),
+    }
   });
 
   await app.listen();
